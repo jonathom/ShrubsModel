b6ab91f9363c609d48ee37b040239b4f89f41763
Paula Scharf <paulascharf@gmx.de>
2021-01-24 21:34:12 +0100

    rebase on main

--- model.py
+++ model.py
@@ -6,15 +6,13 @@ import numpy as np
 
 class ShrubManage(DynamicModel, MonteCarloModel):
     def __init__(self):
-        # this is just a test grid (the dimensions are not right)
+        DynamicModel.__init__(self)
+        # need to fix single shrub patches on bottom margin in initial map
         setclone('initialStateA.map')
         MonteCarloModel.__init__(self)
         DynamicModel.__init__(self)
 
     def premcloop(self):
-        self.initialM = self.readmap('initialStateA')
-        #2=shrubs, 1=grass, 0=empty
-        self.biotop = self.initialM
         self.b1 = 6.8
         self.b2 = 0.387
         self.b3 = 38.8
@@ -26,19 +24,37 @@ class ShrubManage(DynamicModel, MonteCarloModel):
         self.dg = 0.125
         self.theta = 0.8
 
+        #initializing parameters for management practices
+        self.year = 0
+        self.h = 0    # grazing pressure (0 to 1)
+        self.n = 5    # removal event period (in years)
+        self.f = 0.2  # fraction of shrub area removed
+
+        self.initialMap = self.readmap('initialStateA')
+        #2=shrubs, 1=grass, 0=empty
+        self.biotop = self.initialMap
+
         self.shrubExpansion = []
         self.shrubDensity = []
 
     def initial(self):
-        self.biotop = self.initialM
+        self.biotop = self.initialMap
 
     def dynamic(self):
         prevShrubTotal = cellvalue(maptotal(scalar(self.biotop==2)),0)[0]
         self.shrubDensity.append([prevShrubTotal/(200*200),self.currentTimeStep(),self.currentSampleNumber()])
 
+        #mechanical removal event every nth year with fraction f being removed
+        #TODO: Removed shrub cells selected randomly within patch, not one clump at the edge. How to do this?
+        self.year = self.year + 1
+        realization = uniform(1) < self.f
+        if (self.year == self.n):
+            self.shrubRemoved = pcrand(realization, self.shrub)
+            self.biotop = ifthenelse(self.shrubRemoved,0,self.biotop)
+            self.year = 0
+
+
         self.report(self.biotop, "biotop")
-        self.b = scalar(self.biotop)
-        self.report(self.b, "b") # so that type of map is scalar
         random = uniform(1)
         self.grass = self.biotop==1
         weg = empty2grass(self)
@@ -71,11 +87,12 @@ class ShrubManage(DynamicModel, MonteCarloModel):
         self.biotop = ifthenelse(self.shrubDeath, 0, self.biotop)
 
         postShrubTotal = cellvalue(maptotal(scalar(self.biotop==2)),0)[0]
+        # this corresponds to chapter 3.0 "logarithm of shrub cover in year t+1 divided by shrub cover in year t"
         intrinsicGrowth = math.log(postShrubTotal/prevShrubTotal)
         self.shrubExpansion.append([intrinsicGrowth,self.currentTimeStep(),self.currentSampleNumber()])
 
     def postmcloop(self):
-        ''' this is to visualize the shrub growth per timestep (like figure )
+        ''' this is to visualize the shrub growth per timestep (like figure 2b)
         for i in range(nrOfSamples):
             plt.plot([item[1] for item in self.shrubExpansion if item[2] == i],[item[0] for item in self.shrubExpansion if item[2] == i])
         '''
@@ -88,7 +105,7 @@ class ShrubManage(DynamicModel, MonteCarloModel):
                    [item[0] for item in self.shrubDensity if item[2] == 1], '--')
         plt.show()
 
-        names = ['b'] # change back to 'biotop' for original error
+        names = ['biotop']
         sampleNumbers = self.sampleNumbers()
         timesteps = self.timeSteps()
         percentiles = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
@@ -97,9 +114,10 @@ class ShrubManage(DynamicModel, MonteCarloModel):
 
 
 def empty2grass(self):
+    #used von Neumann neighbourhood for all functions (as defined on p.162, 2.2)
     qge = window4total(scalar(self.grass))/4
-    #30x40 is the size of the test biotop
-    pg = maptotal(scalar(self.grass))/(30*40)
+    #200x200 is the size of the test biotop
+    pg = maptotal(scalar(self.grass))/(200*200)
     weg = self.bg * qge + self.theta * pg
     return weg
 
@@ -107,14 +125,14 @@ def grass2empty(self):
     return self.dg
 
 def empty2shrub(self):
-    #used von Neumann neighbourhood type (as defined on p.162, 2.2)
     qse = window4total(scalar(self.shrub))/4
     wes = ((1 - qse) * self.b1 + self.s1) * qse
     return wes
 
 def grass2shrub(self):
+    # including grazing pressure self.h (last sentence in 2.7)
     qsg = window4total(scalar(self.shrub))/4
-    wgs = ((1 - qsg) * self.b2 + self.s2) * qsg
+    wgs = ((1 - qsg) * self.b2 * (1 - self.h) + self.s2) * qsg
     return wgs
 
 def shrub2empty(self):
